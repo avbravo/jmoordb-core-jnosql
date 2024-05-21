1a7f7d7d26b5589884a2d34fc06615b47ad8846d
avbravo <avbravo@avbravo>
2024-05-20 22:55:36 -0500

    1.6.0.100

--- src/main/java/com/jmoordb/core/repository/CrudRepository.java
+++ src/main/java/com/jmoordb/core/repository/CrudRepository.java
@@ -11,12 +11,16 @@ import com.jmoordb.core.annotation.repository.Find;
 import com.jmoordb.core.annotation.repository.Save;
 import com.jmoordb.core.annotation.repository.Update;
 import com.jmoordb.core.annotation.repository.UpdateMany;
+import com.jmoordb.core.context.ContextJMoordbCore;
 import com.jmoordb.core.model.Pagination;
 import com.jmoordb.core.model.Search;
 import com.jmoordb.core.model.Sorted;
 import com.jmoordb.core.processor.model.JmoordbException;
+import com.mongodb.client.ListIndexesIterable;
+import com.mongodb.client.MongoIterable;
 import java.util.List;
 import java.util.Optional;
+import org.bson.Document;
 import org.bson.conversions.Bson;
 
 /**
@@ -24,6 +28,32 @@ import org.bson.conversions.Bson;
  * @author avbravo
  */
 public interface CrudRepository<T, PK> {
+
+    default public void setDynamicDatabase(String dataBase) {
+        ContextJMoordbCore.mongodbdatabase = dataBase;
+    }
+
+    default public String getDynamicDatabase() {
+        if (ContextJMoordbCore.mongodbdatabase == null) {
+            return "";
+        } else {
+            return ContextJMoordbCore.mongodbdatabase;
+        }
+
+    }
+
+    default public void setDynamicCollection(String collection) {
+        ContextJMoordbCore.mongodbcollection = collection;
+    }
+
+    default public String getDynamicCollection() {
+        if (ContextJMoordbCore.mongodbcollection == null) {
+            return "";
+        } else {
+            return ContextJMoordbCore.mongodbcollection;
+        }
+
+    }
 
     @Save
     public Optional<T> save(T t);
@@ -43,16 +73,26 @@ public interface CrudRepository<T, PK> {
     @Find()
     public List<T> findAllPaginationSorted(Pagination pagination, Sorted sorted);
 
-   
     public Optional<T> findByPk(PK id);
-    
+
     @DeleteBy
     public Long deleteByPk(PK id);
-@DeleteMany
+
+    @DeleteMany
     public Long deleteMany(Search search);
-    
+
     @UpdateMany
     public Long updateMany(Bson query, Bson update);
-@CoreException()
-public JmoordbException getJmoordbException();
+
+    @CoreException()
+    public JmoordbException getJmoordbException();
+
+    public String createIndex(Bson bson);
+
+    public void dropIndex(Bson bson);
+
+    public Optional<ListIndexesIterable<Document>> listIndexes();
+    
+     public Optional<MongoIterable<String>> listCollectionNames();
+     
 }
